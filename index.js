c367aeb0c793aab340653109d6e93d24dc33a1c8
Pradeep <pradeepyandamuri532@gmail.com>
2025-07-07 17:18:02 +0530

    index.js changed

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const authRoutes = require("./routes/auth");
 const scheduleTaskReminders = require("./utils/scheduler");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 scheduleTaskReminders();
-
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
